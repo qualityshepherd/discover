bfd3629a095a79f2935de765b18648bd74d2900f
Brine <brian@qualityshepherd.com>
2026-05-22 06:15:13 -0600

    botty

--- worker/analytics-core.js
+++ worker/analytics-core.js
@@ -6,7 +6,7 @@ const BOT_PREFIXES = [
   '/account/', '/api/v1', '/back/', '/bak/', '/billing/', '/cgi-bin/', '/checkout',
   '/conf.d/', '/donate', '/error/', '/etc/', '/file-upload', '/fileupload',
   '/files/', '/form/', '/import/', '/info', '/log/', '/login',
-  '/mcp', '/old/', '/opt/', '/order/', '/php-cgi', '/phpinfo', '/plans/',
+  '/mcp', '/officialsite', '/old/', '/opt/', '/order/', '/php-cgi', '/phpinfo', '/plans/',
   '/proc/', '/register', '/rest/', '/restore/', '/root/', '/shop/', '/sse',
   '/storage/', '/subscribe', '/temp', '/test', '/tmp', '/upload',
   '/v1/', '/v2/', '/v3/', '/var/', '/vendor', '/wallet/', '/webhook/', '/wp-'
